6dbbab9d79487222c2aaa9ee3b5e2d1e4508023b
iurikali <iurisiecz@gmail.com>
2025-09-05 20:49:27 -0300

    primeiro commit

---
+++ Scripts/player_runner.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var velh := 0
+var velv := 0
+var speed := 100
+
+func _physics_process(delta: float) -> void:
+	
+	velh = Input.get_axis("LEFT", "RIGHT")
+	velv = Input.get_axis("UP", "DOWN")
+
+	velocity.x = velh * speed
+	velocity.y = velv * speed
+	move_and_slide()
