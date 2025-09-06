93acede238e59f58a4acc23c91e66cac67528636
iurikali <iurisiecz@gmail.com>
2025-09-06 02:27:49 -0300

    pull

--- Scripts/Runner/runner_shadow.gd
+++ Scripts/Runner/runner_shadow.gd
@@ -1,12 +1,4 @@
 extends Node2D
 
-@export var runner_sprite : AnimatedSprite2D
-@onready var animated: AnimatedSprite2D = $AnimatedSprite2D
-
-func _process(delta: float) -> void:
-	#animated.animation = runner_sprite.animation
-	#animated.set_instance_shader_parameter("")
-	pass
-
-func _on_timer_timeout() -> void:
+func deleta():
 	queue_free()
