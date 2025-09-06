29322ef3632d9ff12d4a10c27edf35abed5f3ba4
elPedr0n <pepe18616@gmail.com>
2025-09-06 14:52:09 -0300

    parallax variavel

--- Game/Scripts/parallax.gd
+++ Game/Scripts/parallax.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var vel = 300.0
+var vel = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +10,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
+	if Global.estagio == 2:
+		vel = 160
+	elif Global.estagio == 3:
+		vel = 220
+	elif Global.estagio == 4:
+		vel = 300
 	position.x -= vel * delta
 	if position.x < - 768:
 		position.x = position.x + 2 * 768
